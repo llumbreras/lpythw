15ab5bf535a74ae8d5d95bab5fd739ead74f43ed
L Lumbreras <llumbreras@gmail.com>
2013-08-01 18:08:42 -0700

    Complete exercise 01

---
+++ ex001.py
@@ -0,0 +1,7 @@
+print "Hello Python"
+print "Hello Again"
+print "This is fun!"
+print 'Python! Listening...'
+print "We'll keep going."
+print "And we'll see the 'path' to follow."
+print 'I "noted" this on my computer.'
